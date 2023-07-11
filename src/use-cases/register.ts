4db05031347c96723c215b5aead2f7bd5376cd7d
Rodrigo Bighetti <robighetti@gmail.com>
2023-07-11 06:10:33 -0300

    tratamentos de solid e error handler

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,5 +1,6 @@
-import {prisma} from "@/lib/prisma"
+import { UsersRepository } from '@/repositories/users-repository'
 import { hash } from 'bcryptjs'
+import { UserAlreadyExistsError } from './errors/user-already-exists-error'
 
 interface RegisterUseCaseProps {
   name: string
@@ -7,23 +8,21 @@ interface RegisterUseCaseProps {
   password: string
 }
 
-export async function registerUseCase({name, email, password}: RegisterUseCaseProps) {
-  const password_hash = await hash(password, 6)
+export class RegisterUseCase {
+  constructor(private usersRepository: UsersRepository) {}
 
-  const userWithSameEmail = await prisma.user.findUnique({
-    where: {
-      email
+  async execute({ name, email, password }: RegisterUseCaseProps) {
+    const password_hash = await hash(password, 6)
+
+    const userWithSameEmail = await this.usersRepository.findByEmail(email)
+    if (userWithSameEmail) {
+      throw new UserAlreadyExistsError()
     }
-  })
-  if (userWithSameEmail) {
-    throw new Error("Email already exists")
-  }
 
-  await prisma.user.create({
-    data: {
+    await this.usersRepository.create({
       name,
       email,
       password_hash,
-    },
-  })
+    })
+  }
 }
